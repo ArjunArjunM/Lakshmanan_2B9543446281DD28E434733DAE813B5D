b32269a551e2bb4ac3ffda7d421f45f83a5d74fa
ArjunArjunM <144428682+ArjunArjunM@users.noreply.github.com>
2023-09-08 13:45:27 +0530

    Add files via upload

---
+++ Leap year.py.py
@@ -0,0 +1,6 @@
+print("Program to find the leap year")
+year = int(input("Enter the year:"))
+if(year%4==0):
+  print("The Given ",year," is leap year")
+else:
+  print("The given ",year, "is not a leap year")
